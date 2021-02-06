b7c8b54fcd55ee2883490daeec5ce901cec12bca
Xamix123 <39437669+Xamix123@users.noreply.github.com>
2021-02-06 11:31:32 +0200

    PE_1_1_0_0
    add decision use cycle.

---
+++ ProjectEuler_1_Multiple_of_3_and_5/public/view/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    var limit = $('#limit').attr("value");
+    var numbers = [];
+    var sum = 0;
+    for(let counter = 0 ; counter < limit; counter++) {
+        if((counter % 3 === 0) || (counter % 5 === 0))
+        {
+            numbers.push(counter);
+            sum+= counter;
+        }
+    }
+
+    
+
+
+    $('#resultCycle').text(sum);
+    $('#resultMath').text(sumMath);
+    $('#numbers').text(numbers);
+});
